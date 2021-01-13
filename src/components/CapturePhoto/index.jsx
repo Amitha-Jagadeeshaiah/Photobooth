4f7cfa29bb7c22965a7e1edf8082c65d9cf8b139
Amitha Jagadeeshaiah <amthajagadeeshaiah.1986@gmail.com>
2021-01-13 17:35:55 +0000

    Added ESLint and changed the Ternary operator pattern

--- src/components/CapturePhoto/index.jsx
+++ src/components/CapturePhoto/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable max-len */
 import React, { createRef } from 'react';
 import Webcam from 'react-webcam';
 import { Link } from 'react-router-dom';
@@ -7,15 +9,15 @@ import lilacPattern from '../../Images/pattern-lilac1.svg';
 import tocalogo from '../../Images/tocalogo-lilac.svg';
 import cameraIcon from '../../Images/cameraIcon.svg';
 
-const { 
-    photoContainer, 
-    headerPattern, 
+const {
+    photoContainer,
+    headerPattern,
     imageCaptureContainer,
     image,
-    imageCapture, 
+    imageCapture,
     timeInSec,
     webcam,
-    cameraImage, 
+    cameraImage,
     logo,
     footer,
     photoButton,
@@ -26,112 +28,137 @@ const {
 } = Styles;
 
 export default class CapturePhoto extends React.Component {
-    constructor(){
+
+    constructor() {
+
         super();
-        
+
         this.state = {
-            startTimerClicked : false,
+            startTimerClicked: false,
             seconds: 7,
             timerSec: 4,
             imgSrc: null
-        }
+        };
         this.timer = 0;
         this.webcamRef = createRef();
+
     }
 
-    startTimer = () => {
+    startTimer=()=>{
+
         if (this.timer === 0 && this.state.seconds >= 0) {
+
             this.timer = setInterval(this.countDown, 1000);
+
         }
+
     }
 
-    countDown = ()=>{
-        const {seconds, timerSec} = this.state;
+    countDown=()=>{
+
+        const { seconds, timerSec } = this.state;
         const secondsNext = seconds - 1;
         if (seconds === 0) {
+
             // No time left, take a picture!
             this.captureImage();
             clearInterval(this.timer);
+
         } else {
+
             // Remove one second, set state so a re-render happens.
             this.setState({
                 startTimerClicked: true,
                 seconds: secondsNext,
                 timerSec: timerSec - 1
             });
+
         }
+
     }
 
-    captureImage = ()=>{
+    captureImage = () => {
+
         const imgSrc = this.webcamRef.current.getScreenshot();
         this.setState({
             imgSrc
         });
+
     }
 
     retakePhoto = () => {
+
         this.timer = 0;
-        this.setState ((prevState) =>{
-            return {
-                startTimerClicked : prevState.startTimerClicked && false,
-                seconds: 7,
-                timerSec: 4,
-                imgSrc: prevState.imgSrc !== null && null
-            }
-        })
+        this.setState(prevState => ({
+            startTimerClicked: prevState.startTimerClicked && false,
+            seconds: 7,
+            timerSec: 4,
+            imgSrc: prevState.imgSrc !== null && null
+        }));
+
     }
 
-    render(){
-        return(
+    render() {
+
+        return (
             <div className={photoContainer}>
-                <div className={headerPattern} style={{ backgroundImage: `url('${whitePattern} ')` }} />
-                <div className={imageCaptureContainer}  onClick = {this.startTimer}>
+                <div className={headerPattern}
+                    style={{ backgroundImage: `url('${whitePattern} ')` }}
+                />
+                <div className={imageCaptureContainer}
+                    onClick={this.startTimer}
+                >
                     {
-                        this.state.imgSrc ? 
-                            <img className={image} src={this.state.imgSrc} alt='selfie'/>
-                        : this.state.startTimerClicked ? 
-                                this.state.seconds > 3 ? 
-                                    <span className={timeInSec}>{this.state.timerSec}</span>
+                        this.state.imgSrc
+                            ? <img className={image} src={this.state.imgSrc} alt='selfie' />
+                            : this.state.startTimerClicked
+                                ? this.state.seconds > 3
+                                    ? <span className={timeInSec}>{this.state.timerSec}</span>
+                                    : (
+                                        <Webcam audio={false}
+                                            ref={this.webcamRef}
+                                            screenshotFormat="image/jpeg"
+                                            imageSmoothing='true'
+                                            className={webcam}
+                                        />
+                                    )
                                 : (
-                                    <Webcam audio={false}
-                                    ref={this.webcamRef}
-                                    screenshotFormat="image/jpeg"
-                                    imageSmoothing='true'
-                                    className={webcam}
-                                    />
+                                    <div className={imageCapture}>
+                                        <img className={cameraImage} src={cameraIcon} alt='camera' />
+                                        <h3>Tap</h3>
+                                    </div>
                                 )
-                            : (
-                                <div className={imageCapture}>
-                                    <img className={cameraImage} src = {cameraIcon} alt = 'camera' />
-                                    <h3>Tap</h3>
-                                </div>
-                            )
                     }
                     <div className={logo}>
-                        <img src={tocalogo} alt='logo'/>
+                        <img src={tocalogo} alt='logo' />
                     </div>
                 </div>
-                {this.state.imgSrc ?
-                    (
+                {this.state.imgSrc
+                    ? (
                         <div className={footer}>
-                            <button className={retakeButton} onClick = {this.retakePhoto}>Retake</button>
+                            <button className={retakeButton} onClick={this.retakePhoto}>Retake</button>
                             <Link to={{
                                 pathname: '/sendText'
-                            }}><button className={sendTextButton}>Send via Text</button></Link>
+                            }}
+                            ><button className={sendTextButton}>Send via Text</button>
+                            </Link>
                         </div>
                     )
-                :   
-                    (
+                    : (
                         <div className={footer}>
                             <button className={photoButton}>Photo</button>
                             <Link to={{
                                 pathname: '/gif'
-                            }}><button className={gifButton}>Gif</button></Link>
+                            }}
+                            ><button className={gifButton}>Gif</button>
+                            </Link>
                         </div>
                     )
                 }
                 <div className={footerPattern} style={{ backgroundImage: `url('${lilacPattern} ')` }} />
             </div>
-        )
+        );
+
     }
+
 }
--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -3,35 +3,38 @@ import { Link } from 'react-router-dom';
 import Styles from './index.module.css';
 import backgroundImage from '../../Images/bg-pattern-01.svg';
 import backgroundImage1 from '../../Images/pattern-white1.svg';
-import TocaLogo from '../../Images/tocalogo-turquoise.svg'
+import TocaLogo from '../../Images/tocalogo-turquoise.svg';
 
-const { 
-    homepageContainer, 
-    headerPattern, 
-    header, 
-    footer, 
-    footerPattern 
+const {
+    homepageContainer,
+    headerPattern,
+    header,
+    footer,
+    footerPattern
 } = Styles;
 
-const HomePage = () => {
-    return(
-        <div className={homepageContainer}>
-            <div className={headerPattern} style={{ backgroundImage: `url('${backgroundImage} ')` }} />
-            <Link to = {{
-                pathname: '/photo'
-            }}>
-                <div className={header}>
-                    <h2>Tap to</h2>
-                    <h1>Start</h1>
-                </div>
-            </Link>
-            <div className={footer}>
-                <img src = {TocaLogo} alt="logo" />
+const HomePage = () => (
+    <div className={homepageContainer}>
+        <div className={headerPattern}
+            style={{ backgroundImage: `url('${backgroundImage} ')` }}
+        />
+        <Link to = {{
+            pathname: '/photo'
+        }}
+        >
+            <div className={header}>
+                <h2>Tap to</h2>
+                <h1>Start</h1>
             </div>
-            <div className={footerPattern} style={{ backgroundImage: `url('${backgroundImage1} ')` }} />
+        </Link>
+        <div className={footer}>
+            <img src = {TocaLogo} alt="logo" />
         </div>
-    )
-}
+        <div className={footerPattern}
+            style={{ backgroundImage: `url('${backgroundImage1} ')` }}
+        />
+    </div>
+);
 
 export default HomePage;
 

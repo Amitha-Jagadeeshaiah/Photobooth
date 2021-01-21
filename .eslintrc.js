423a2e267011a2e055ec21c3474ba9a66b0df5e6
Amitha-Jagadeesh <Amitha-Jagadeesh@users.noreply.github.com>
2021-01-21 17:40:10 +0000

    Feature/gif timer integration and multiple image capture (#4)
    * WIP - Implementation of  Design, Timer Integration and Multiple Image capture for GIF component
    
    * Addressed comments.
    
    * Adding new line
    
    * remove eslintcache file
    
    * commit eslint cache
    
    * Working Draft - implementation of GIF
    
    * WorkWorking Draft - changes made to remove console log and onClick
    
    * Working Draft - implementation of GIF
    
    * Working Draft - changes made to remove console log and onClick
    
    * Added gitshot library
    
    * modified css
    
    * Working Draft - Addressed review comments for GIF component
    
    * Working Draft - Addressed review comments for GIF component and removed gifshot file
    
    Co-authored-by: Amitha Jagadeeshaiah <amthajagadeeshaiah.1986@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports= {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'parser': 'babel-eslint',
+    'parserOptions': {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    'plugins': [
+        'react'
+    ],
+    rules: {
+        indent: [ 'warn', 4, { SwitchCase: 1 } ],
+        quotes: [ 'warn', 'single' ],
+        semi: 'warn',
+        'import/no-default-export': 'off',
+        'arrow-body-style': 'warn',
+        'no-debugger': [ 0 ],
+        'padded-blocks': 'warn',
+        'comma-dangle': [ 'error', 'never' ],
+        'no-trailing-spaces': 'warn',
+        'max-len': [ 2, 125, 4, {'ignoreUrls': true} ],
+        'function-paren-newline': 'warn',
+        'block-spacing': 'warn',
+        'no-unused-vars': 'warn',
+        'no-multi-spaces': 'warn',
+        'eol-last': 'warn',
+        'no-multiple-empty-lines': 'warn',
+        'import/prefer-default-export': 'off',
+        'no-confusing-arrow': 'warn',
+        'no-extra-boolean-cast': 0,
+        'no-use-before-define': 'warn',
+        'object-curly-newline': 'warn',
+        'no-nested-ternary': [ 0 ],
+        'no-restricted-syntax': [ 'off' ],
+        'arrow-parens': [ 1, 'as-needed', { requireForBlockBody: true } ],
+        'prefer-destructuring': [ 'warn', { object: true, array: true } ],
+        'array-bracket-spacing': [ 'warn', 'always' ],
+        'react/jsx-indent': [ 1, 4 ],
+        'react/jsx-filename-extension': [ 1 ],
+        'react/jsx-closing-tag-location': [ 0 ],
+        'react/jsx-one-expression-per-line': [ 0, { allow: 'single-child' } ],
+        'react/jsx-indent-props': [ 1, 4 ],
+        'react/jsx-curly-spacing': [ 0 ],
+        'react/button-has-type': [ 1 ],
+        'react/jsx-boolean-value': [ 1 ],
+        'react/jsx-tag-spacing': [ 1 ],
+        'react/prop-types': [ 1 ],
+        'react/self-closing-comp': [ 1 ],
+        'react/jsx-closing-bracket-location': [ 1 ],
+        'react/jsx-props-no-spreading': [ 0 ],
+        'jsx-a11y/no-static-element-interactions': [ 0 ],
+        'jsx-a11y/click-events-have-key-events': [ 0 ],
+        'jsx-a11y/interactive-supports-focus': [ 0 ],
+        'jsx-a11y/label-has-associated-control': [ 0 ],
+        'jsx-a11y/no-noninteractive-element-interactions': [ 0 ],
+        'jsx-a11y/no-noninteractive-element-to-interactive-role': [ 0 ]
+    }
+};
---
+++ src/components/CreateGIF/index.jsx
@@ -0,0 +1,319 @@
+import React, { createRef } from 'react';
+import ProgressBar from 'react-customizable-progressbar';
+import Webcam from 'react-webcam';
+import { Link } from 'react-router-dom';
+import Styles from './index.module.css';
+import whitePattern from '../../Images/GIF-header-pattern.svg';
+import lilacPattern from '../../Images/pattern-lilac2.svg';
+import tocalogo from '../../Images/tocalogo-pink.svg';
+import videoImage from '../../Images/gif-icon.svg';
+import videoPlayButton from '../../Images/play-button.svg';
+import gifshot from 'gifshot';
+
+const {
+    gifContainer,
+    headerPattern,
+    imageCaptureContainer,
+    gifImageContainer,
+    image,
+    playVideo,
+    multiImageCapture,
+    progressbar,
+    videoCapture,
+    captureVideoImage,
+    logo,
+    footer,
+    photoButton,
+    gifButton,
+    retakeButton,
+    sendTextButton,
+    footerPattern
+} = Styles;
+
+export default class CreateGIF extends React.Component {
+
+    constructor(){
+
+        super();
+        this.state = {
+            startTimerClicked : false,
+            seconds: 7,
+            timerSec: 4,
+            result: 0,
+            imgSrc: [],
+            gifVideo: ''
+        };
+        this.timer = 0;
+        this.webcamRef = createRef();
+
+    }
+
+    startTimer = () => {
+
+        if (this.timer === 0 && this.state.seconds > 0) {
+
+            this.timer = setInterval(this.countDown, 1000);
+
+        }
+
+    }
+
+    countDown = () => {
+
+        const { seconds, timerSec } = this.state;
+        const secondsNext = seconds - 1;
+
+        if(this.state.gifVideo) {
+
+            if(secondsNext > 0) {
+
+                //play gif for few seconds
+                this.setState({
+                    startTimerClicked: true,
+                    seconds: secondsNext
+                });
+
+            } else {
+
+                //stop playing gif once timer is stopped
+                clearInterval(this.timer);
+                this.timer = 0;
+                this.setState({
+                    startTimerClicked: false,
+                    seconds: 7,
+                    timerSec: 4,
+                    result: 0
+                });
+
+            }
+
+        } else {
+
+            if(secondsNext > 4) {
+
+                //let user get ready to click selfies
+                this.setState({
+                    startTimerClicked: true,
+                    seconds: secondsNext
+                });
+
+            } else {
+
+                if(secondsNext <= 4 && secondsNext > 0){
+
+                    //take picture every second
+                    const result = this.secondsToPercentages(secondsNext, timerSec);
+                    this.captureImage(secondsNext, result);
+
+                } else {
+
+                    //once the timer is stopped, the GIF is created
+                    clearInterval(this.timer);
+                    this.createGIF();
+
+                }
+
+            }
+
+        }
+
+    }
+
+    //calculate timer progress for progress bar
+    secondsToPercentages = (secondsNext, timerSec) => {
+
+        const result = 100 - (((secondsNext - 1) * 100) / timerSec);
+        if(result > 100) {
+
+            return 100;
+
+        }
+        return result;
+
+    }
+
+    captureImage = (secondsNext, result)=>{
+
+        const imageSrc = this.webcamRef.current.getScreenshot();
+        this.setState(prevState => ({
+            seconds: secondsNext,
+            result,
+            imgSrc: [ ...prevState.imgSrc , imageSrc ]
+        }));
+
+    }
+
+    //create GIF from the clicked images
+    createGIF = () => {
+
+        gifshot.createGIF({
+            gifWidth: 750,
+            gifHeight: 600,
+            interval: 0.6,
+            images: this.state.imgSrc
+        }, (obj) => {
+
+            if (!obj.error) {
+
+                const {image} = obj;
+                this.timer = 0;
+                this.setState({
+                    startTimerClicked: false,
+                    gifVideo : image,
+                    seconds: 7,
+                    timerSec: 4,
+                    result: 0
+                });
+
+            }
+
+        });
+
+    }
+
+    retakePhoto = () => {
+
+        this.timer = 0;
+        this.setState({
+            startTimerClicked : false,
+            seconds: 7,
+            timerSec: 4,
+            result: 0,
+            gifVideo: '',
+            imgSrc: []
+        });
+
+    }
+
+    displayHeaderPattern() {
+
+        return !this.state.startTimerClicked && this.state.imgSrc.length === 0
+            ? headerPattern
+            : null;
+
+    }
+
+    render() {
+
+        return(
+            <div className={gifContainer}>
+                <div className={this.displayHeaderPattern()}
+                    style={{ backgroundImage: `url('${whitePattern} ')` }}
+                />
+                <div className={imageCaptureContainer}
+                    onClick={this.startTimer}
+                >
+                    {
+                        !this.state.gifVideo && (
+                            <Webcam audio
+                                ref={this.webcamRef}
+                                screenshotFormat="image/jpeg"
+                                imageSmoothing='true'
+                            />
+                        )
+                    }
+                    {
+                        (this.state.startTimerClicked && !this.state.gifVideo)
+                            ? (
+                                <div className={multiImageCapture}>
+                                    { (this.state.seconds < 5) &&
+                                    <ProgressBar
+                                        progress={this.state.result}
+                                        radius={100}
+                                        strokeColor={'#FFFFFF'}
+                                        strokeWidth={5}
+                                        trackStrokeColor={'none'}
+                                        className={progressbar}
+                                    />
+                                    }
+                                </div>
+                            )
+                            : (this.state.startTimerClicked && this.state.gifVideo)
+                                ? (
+                                    <div className={gifImageContainer}>
+                                        <img className={image}
+                                            src={this.state.gifVideo}
+                                            alt='gif'
+                                        />
+                                    </div>
+                                )
+                                : (!this.state.startTimerClicked && this.state.gifVideo)
+                                    ? (
+                                        <div className={gifImageContainer}>
+                                            <img className={image}
+                                                src={this.state.imgSrc[0]}
+                                                alt='selfie'
+                                            />
+                                            <img className={playVideo}
+                                                src = {videoPlayButton}
+                                                alt = 'gif'
+                                            />
+                                        </div>
+                                    )
+                                    : (
+                                        <div className={videoCapture}>
+                                            <img className={captureVideoImage}
+                                                src={videoImage}
+                                                alt='video'
+                                            />
+                                            <h3>Tap</h3>
+                                        </div>
+                                    )
+                    }
+                    <div className={logo}>
+                        <img src={tocalogo} alt='logo' />
+                    </div>
+                </div>
+                {
+                    !this.state.gifVideo
+                        ? (
+                            <div className={footer}>
+                                <Link to={{
+                                    pathname: '/photo'
+                                }}
+                                >
+                                    <button className={photoButton}
+                                        type="button"
+                                    >
+                                        Photo
+                                    </button>
+                                </Link>
+                                <button className={gifButton}
+                                    type="button"
+                                >
+                                    Gif
+                                </button>
+                            </div>
+                        )
+                        : (
+                            <div className={footer}>
+                                <button className={retakeButton}
+                                    type="button"
+                                    onClick={this.retakePhoto}
+                                >
+                                    Retake
+                                </button>
+                                <Link to={{
+                                    pathname: '/sendText',
+                                    data:this.state.gifVideo
+                                }}
+                                >
+                                    <button className={sendTextButton}
+                                        type="button"
+                                    >
+                                        Send via Text
+                                    </button>
+                                </Link>
+                            </div>
+                        )
+                }
+                <div className={footerPattern}
+                    style={{ backgroundImage: `url('${lilacPattern} ')` }}
+                />
+            </div>
+        );
+
+    }
+
+}
+

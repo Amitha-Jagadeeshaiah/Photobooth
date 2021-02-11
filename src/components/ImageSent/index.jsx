1b4805e18dc3f5fafa9f335f94c0ff688752149d
Amitha-Jagadeesh <Amitha-Jagadeesh@users.noreply.github.com>
2021-02-11 10:41:19 +0000

    Working Draft - Implementation of Image Sent Component (#7)
    Co-authored-by: Amitha Jagadeeshaiah <amthajagadeeshaiah.1986@gmail.com>

---
+++ src/components/ImageSent/index.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import Styles from './index.module.css';
+import { ReactComponent as MobileIcon} from '../../Images/mobileIcon.svg';
+
+const {
+    imageContainer,
+    dataContainer,
+    headerContainer,
+    header1,
+    header2,
+    mobileImageContainer,
+    controlButtonContainer,
+    btn
+} = Styles;
+
+export default class ImageSent extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            timerseconds: 10,
+            btnClicked: false
+        };
+        this.timer = 0;
+
+    }
+
+    startTimer = () => {
+
+        if (this.timer === 0 && this.state.timerseconds > 0) {
+
+            this.timer = setInterval(this.countDown, 1000);
+
+        }
+
+    }
+
+    countDown = () => {
+
+        if(!this.state.btnClicked) {
+
+            const { timerseconds } = this.state;
+            const secondsNext = timerseconds - 1;
+
+            if (timerseconds === 0) {
+
+                // No time left, take back to home page!
+                clearInterval(this.timer);
+                this.props.history.push('/');
+
+            } else {
+
+                // Remove one second, set state so a re-render happens.
+                this.setState({
+                    timerseconds: secondsNext
+                });
+
+            }
+
+        }
+
+    }
+
+    handleClick = async () => {
+
+        await this.componentWillUnmount();
+        this.props.history.push('/photo');
+
+    }
+
+    componentDidMount() {
+
+        this.startTimer();
+
+    }
+
+    componentWillUnmount() {
+
+        this.timer = 0;
+        this.setState({
+            btnClicked: true
+        });
+
+    }
+
+
+    render(){
+
+        return(
+            <div className={imageContainer}>
+                <div className={dataContainer}>
+                    <div className={headerContainer}>
+                        <p className={header1}>Sent</p>
+                        <p className={header2}>
+                            The picture has been sent to your mobile
+                        </p>
+                    </div>
+                    <div className={mobileImageContainer}>
+                        <MobileIcon />
+                    </div>
+                    <div className={controlButtonContainer} onClick={this.handleClick}>
+                        <button type="button">
+                            <span className={btn}>Take Another</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+
+}
+

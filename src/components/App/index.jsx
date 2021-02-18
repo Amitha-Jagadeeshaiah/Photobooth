eeb4ec7a4701eb293c17805e24e319f541ad49d2
Amitha Jagadeeshaiah <amthajagadeeshaiah.1986@gmail.com>
2021-02-18 23:10:19 +0000

    Working Draft - Code changes made to fix orientation bug

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import DeviceOrientation, { Orientation } from 'react-screen-orientation';
 import HomePage from '../HomePage';
 import CapturePhoto from '../CapturePhoto';
 import CreateGIF from '../CreateGIF';
@@ -11,20 +10,16 @@ import ImageSent from '../ImageSent';
 export default function App() {
 
     return (
-        <DeviceOrientation lockOrientation={'portrait'}>
-            <Orientation orientation='portrait' alwaysRender={false}>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path = '/' component = { HomePage } exact />
-                        <Route path = '/photo' component = { CapturePhoto } exact />
-                        <Route path = '/gif' component = { CreateGIF } exact />
-                        <Route path = '/sendText' component = { SendText } exact />
-                        <Route path = '/signUp' component = { SignUp } exact />
-                        <Route path = '/imageSent' component = { ImageSent } exact />
-                    </Switch>
-                </BrowserRouter>
-            </Orientation>
-        </DeviceOrientation>
+        <BrowserRouter>
+            <Switch>
+                <Route path = '/' component = { HomePage } exact />
+                <Route path = '/photo' component = { CapturePhoto } exact />
+                <Route path = '/gif' component = { CreateGIF } exact />
+                <Route path = '/sendText' component = { SendText } exact />
+                <Route path = '/signUp' component = { SignUp } exact />
+                <Route path = '/imageSent' component = { ImageSent } exact />
+            </Switch>
+        </BrowserRouter>
     );
 
 }
